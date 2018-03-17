e738583d076ab5091f5244ea927ed130a3135d9b
Nadeeth Lansakara <nadeethlansakara@Nadeeths-MBP.hub>
2018-03-17 23:07:01 +1300

    Reverting back to normal js dev env for the sake of smiplicity

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,13 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-const babel = require('gulp-babel');
  
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
- 
-gulp.task('js', () =>
-    gulp.src('js/custom/app.js')
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(gulp.dest('js/dist'))
-);
 
 gulp.task('watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
-  gulp.watch('./js/custom/**/*.js', ['js']);
 });
